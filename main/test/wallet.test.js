ec8fbd7784fab84aed0f14fb7b83fcb545343336
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-28 17:09:41 +0000

    Test for checking for wrong signature passing

--- main/test/wallet.test.js
+++ main/test/wallet.test.js
@@ -7,13 +7,23 @@ const luc = wallet.create();
 
 group('Wallet', () => {
   group('method: crypto.createVerify', () => {
-    check('return true for properly signed data', () => {
+    check('return true for correct data signature', () => {
       const dataToSign = 'some data to sign';
     	const privateKey = wallet.getNodePrivateKey(ces.publicKey, ces.privateKey);
       const publicKey = wallet.getNodePublicKey(ces.publicKey);
       const signature = createSign('SHA256').update(dataToSign).sign(privateKey, 'hex');
       const isValid = createVerify('SHA256').update(dataToSign).verify(publicKey, signature, 'hex');
       verify.isTrue(isValid);
+    });
+  });
+  group('method: crypto.createVerify', () => {
+    check('return false for wrong data signature', () => {
+      const dataToSign = 'some data to sign';
+	    const privateKey = wallet.getNodePrivateKey(ces.publicKey, luc.privateKey);
+      const publicKey = wallet.getNodePublicKey(ces.publicKey);
+      const signature = createSign('SHA256').update(dataToSign).sign(privateKey, 'hex');
+      const isValid = createVerify('SHA256').update(dataToSign).verify(publicKey, signature, 'hex');
+      verify.isFalse(isValid);
     });
   });
 });

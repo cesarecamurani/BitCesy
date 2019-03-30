d5e1913d6be16313fb7cb8df2909b0512debfd51
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-30 16:05:31 +0000

    More detailde comments in Wallet

--- main/src/wallet.js
+++ main/src/wallet.js
@@ -4,6 +4,12 @@ const CONFIG = exports.CONFIG = ({BLOCK_DIFFICULTY: 2, BLOCK_REWARD: 50});
 class Wallet {
 
   constructor(opts={}) {
+    /*
+    Object.assign() copies the values (of all enumerable own properties)
+    from one or more source objects to a target object. 
+    It has a signature of Object.assign(target, ...sources).
+    The target object is the first parameter and is also used as the return value.
+    */
     Object.assign(this, {
       publicKey: null,
       privateKey: null,
@@ -17,7 +23,9 @@ class Wallet {
     secp256k1 refers to the parameters of the elliptic curve used in Bitcoin's public-key cryptography
     */
     const keypair = createECDH('secp256k1');
-    // Generates private and public EC Diffie-Hellman key values, and returns the public key in the specified format and encoding
+    /* Generates private and public EC Diffie-Hellman key values,
+    and returns the public key in the specified format and encoding
+    */
     keypair.generateKeys();
     return new Wallet({
       publicKey:  keypair.getPublicKey ('hex'),
@@ -27,12 +35,20 @@ class Wallet {
 
   getNodePrivateKey(user, key) {
     const t = '308184020100301006072a8648ce3d020106052b8104000a046d306b0201010420';
+    /*
+    Buffer.from takes the values passed as strings and encode them in hex base
+    then .toString convert the result in base64
+    */
     const k = Buffer.from(t + key + 'a144034200' + user, 'hex').toString('base64');
     return `-----BEGIN PRIVATE KEY-----\n${k}\n-----END PRIVATE KEY-----`;
   }
 
   getNodePublicKey(user) {
     const t = '3056301006072a8648ce3d020106052b8104000a034200';
+    /*
+    Buffer.from takes the values passed as strings and encode them in hex base
+    then .toString convert the result in base64
+    */
     const k = Buffer.from(t + user,  'hex').toString('base64');
     return `-----BEGIN PUBLIC KEY-----\n${k}\n-----END PUBLIC KEY-----`;
   }
